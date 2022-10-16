315d9383cd48b7f25de6b88781cc2b2a78deb2b0
Kudzik <kudzik@outlook.com>
2022-10-16 17:46:12 +0200

    modified:   001_Hello_World/Program.cs

--- 001_Hello_World/Program.cs
+++ 001_Hello_World/Program.cs
@@ -1,19 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int i = 6;
+string z = "Hello";
+bool b = false;
+
+TellMeType(i);
 
 
-int x = 2;
-Console.WriteLine(x);  // 2;
-addTwo(x);
-Console.WriteLine(x);  // nadal 2;
-
-void addTwo(int i)
+static void TellMeType(object o)
 {
-    i=i+1;
-    Console.WriteLine(i); // 3;
+    switch (o)
+    {
+        case null:
+            System.Console.WriteLine("Null!");
+            break;
+        case DateTime _:
+            Console.WriteLine("It's a DateTime");
+            break; // 
+        case int i when i == 5:
+            System.Console.WriteLine("Int równy 5");
+            break;
+        case int i:
+            System.Console.WriteLine("Tylko int");
+            break;
+        case string s:
+            System.Console.WriteLine("typ string");
+            break;
+        default:
+            System.Console.WriteLine("Nieznany typ");
+            break;
+    }
 }
-
-// Kolejność wykonania instrukcji
-// 2
-// 3
-// 2
