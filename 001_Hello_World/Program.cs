0d7ee05108ed4f7f89d3a2cfa23ee4021048770d
Kudzik <kudzik@outlook.com>
2022-10-16 11:56:00 +0200

    Update

--- 001_Hello_World/Program.cs
+++ 001_Hello_World/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+int x = 2;
+Console.WriteLine(x);  // 2;
+addTwo(x);
+Console.WriteLine(x);  // nadal 2;
+
+void addTwo(int i)
+{
+    i=i+1;
+    Console.WriteLine(i); // 3;
+}
+
+// Kolejność wykonania instrukcji
+// 2
+// 3
+// 2
