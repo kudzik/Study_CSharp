ebeadd7b006b58dcf9bde00ee863c420444898fc
Kudzik <kudzik@outlook.com>
2022-10-20 12:29:31 +0200

    modified:   001_Hello_World/Program.cs

--- 001_Hello_World/Program.cs
+++ 001_Hello_World/Program.cs
@@ -1,32 +1,34 @@
 ﻿
-int i = 6;
-string z = "Hello";
-bool b = false;
+// Klasa worker zawiera to co Person dodatkowo dodaje nową własność
 
-TellMeType(i);
+Worker worker = new() { Age = 30 };
 
 
-static void TellMeType(object o)
+class Person
 {
-    switch (o)
+    string? Name { get; set; }
+    public int Age { get; set; }
+
+    public virtual void SayHello()
     {
-        case null:
-            System.Console.WriteLine("Null!");
-            break;
-        case DateTime _:
-            Console.WriteLine("It's a DateTime");
-            break; // 
-        case int i when i == 5:
-            System.Console.WriteLine("Int równy 5");
-            break;
-        case int i:
-            System.Console.WriteLine("Tylko int");
-            break;
-        case string s:
-            System.Console.WriteLine("typ string");
-            break;
-        default:
-            System.Console.WriteLine("Nieznany typ");
-            break;
+        System.Console.WriteLine("Hello I'm Person");
+    }
+
+    public int WorkPrefix => 0; // Równoważne z 
+    public int WorkPrefix { get { return 0; } }
+
+}
+
+class Worker : Person
+{
+    public int WorkerID { get; set; }
+
+    // Przesłonięta metoda z klasy bazowej
+    public override void SayHello()
+    {
+        System.Console.WriteLine("Hello I'm Worker");
+        //base.SayHello() // Wywołanie metody z klasy bazowej
+
     }
 }
+
